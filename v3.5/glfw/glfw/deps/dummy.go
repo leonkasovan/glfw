615457b7270327bfcba6fa989982cbc343b5efa2
Novan(Steamdeck) <dhani.novan@steamdeck>
2025-01-18 12:11:24 +0700

    add dummy

--- v3.5/glfw/glfw/deps/dummy.go
+++ v3.5/glfw/glfw/deps/dummy.go
@@ -8,5 +8,5 @@ import (
 	// Prevent go tooling from stripping out the c source files.
 	_ "github.com/leonkasovan/glfw/v3.5/glfw/glfw/deps/glad"
 	_ "github.com/leonkasovan/glfw/v3.5/glfw/glfw/deps/mingw"
-	_ "github.com/leonkasovan/glfw/v3.5/glfw/glfw/deps/vs2008"
+	_ "github.com/leonkasovan/glfw/v3.5/glfw/glfw/deps/wayland"
 )
